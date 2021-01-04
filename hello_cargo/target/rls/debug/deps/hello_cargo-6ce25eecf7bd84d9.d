01b507c3deda65cbf969e8e81986d95e9a22e916
Andre Larberg <andilar@Geonosis.fritz.box>
2021-01-04 14:28:19 +0100

    added hello world in rust

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-6ce25eecf7bd84d9.d
@@ -0,0 +1,5 @@
+/Users/andilar/Documents/coding/rustiland/hello_cargo/target/rls/debug/deps/hello_cargo-6ce25eecf7bd84d9.rmeta: src/main.rs
+
+/Users/andilar/Documents/coding/rustiland/hello_cargo/target/rls/debug/deps/hello_cargo-6ce25eecf7bd84d9.d: src/main.rs
+
+src/main.rs:
