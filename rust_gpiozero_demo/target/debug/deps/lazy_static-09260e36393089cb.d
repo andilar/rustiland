a901d76e68ad74b33fd523c0d0da7ce9306f1aa1
Andre Larberg <andilar@gmail.com>
2021-01-05 18:01:05 +0100

    raspberry rust test with blinking LED

--- rust_gpiozero_demo/target/debug/deps/lazy_static-09260e36393089cb.d
+++
@@ -1,8 +0,0 @@
-/Users/andilar/Documents/coding/rustiland/rust_gpiozero_demo/target/debug/deps/lazy_static-09260e36393089cb.rmeta: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/Users/andilar/Documents/coding/rustiland/rust_gpiozero_demo/target/debug/deps/liblazy_static-09260e36393089cb.rlib: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/Users/andilar/Documents/coding/rustiland/rust_gpiozero_demo/target/debug/deps/lazy_static-09260e36393089cb.d: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
-/Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
