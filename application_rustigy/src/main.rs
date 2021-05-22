c139ee34ebeed17c9df699012d577426db177853
andilar <andilar@gmail.com>
2021-05-22 17:00:09 +0200

    multithreading
    playing with multithreading

--- application_rustigy/src/main.rs
+++ application_rustigy/src/main.rs
@@ -1,18 +1,31 @@
 // rust-lang
+use crossbeam;
 
 fn main() {
     println!("application rustigy started! ");
 
-    let mut x = 49865547671119i64;
-    let mut done = false;
+    let arr = &[1, 25, -4, 10, 1223,1,112332,123];
+    let max = find_max(arr);
+    assert_eq!(max, Some(25));
+}
 
-    while !done {
-        x += x -5;
-
-        println!("{}", x);
-
-        if x % 13 == 0{
-            done = true;
-        }
+fn find_max(arr: &[i32]) -> Option<i32> {
+    const THRESHOLD: usize = 2;
+  
+    if arr.len() <= THRESHOLD {
+        return arr.iter().cloned().max();
     }
+
+    let mid = arr.len() / 2;
+    let (left, right) = arr.split_at(mid);
+  
+    crossbeam::scope(|s| {
+        let thread_l = s.spawn(|_| find_max(left));
+        let thread_r = s.spawn(|_| find_max(right));
+  
+        let max_l = thread_l.join().unwrap()?;
+        let max_r = thread_r.join().unwrap()?;
+  
+        Some(max_l.max(max_r))
+    }).unwrap()
 }
