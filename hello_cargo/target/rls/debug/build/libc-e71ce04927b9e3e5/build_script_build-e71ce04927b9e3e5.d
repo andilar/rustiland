2184fd3d18bf8420796e0e2a65a5e19ba77ef016
andilar <andilar@gmail.com>
2021-11-30 21:22:10 +0100

    added dependency

---
+++ hello_cargo/target/rls/debug/build/libc-e71ce04927b9e3e5/build_script_build-e71ce04927b9e3e5.d
@@ -0,0 +1,5 @@
+/Users/andilar/Documents/GitHub/rustiland/hello_cargo/target/rls/debug/build/libc-e71ce04927b9e3e5/build_script_build-e71ce04927b9e3e5: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
+
+/Users/andilar/Documents/GitHub/rustiland/hello_cargo/target/rls/debug/build/libc-e71ce04927b9e3e5/build_script_build-e71ce04927b9e3e5.d: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
+
+/Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs:
---
+++ hello_cargo/target/rls/debug/deps/lazy_static-890aa9dffdd68514.d
@@ -0,0 +1,6 @@
+/Users/andilar/Documents/GitHub/rustiland/hello_cargo/target/rls/debug/deps/lazy_static-890aa9dffdd68514.rmeta: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/andilar/Documents/GitHub/rustiland/hello_cargo/target/rls/debug/deps/lazy_static-890aa9dffdd68514.d: /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
+/Users/andilar/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
