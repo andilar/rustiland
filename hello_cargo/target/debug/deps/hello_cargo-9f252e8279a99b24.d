21727252a86ab860774e9fa554cd5dc917881b83
andilar <andilar@gmail.com>
2023-07-20 09:32:18 +0200

    deleted binary

--- hello_cargo/target/debug/deps/hello_cargo-9f252e8279a99b24.d
+++
@@ -1,5 +0,0 @@
-C:\Users\I009847\Documents\code\rustiland\hello_cargo\target\debug\deps\hello_cargo-9f252e8279a99b24.rmeta: src\main.rs
-
-C:\Users\I009847\Documents\code\rustiland\hello_cargo\target\debug\deps\hello_cargo-9f252e8279a99b24.d: src\main.rs
-
-src\main.rs:
