f28b788de9e76eeafd28b6bbc5e4455526e88790
andilar <andilar@gmail.com>
2024-08-28 10:39:24 +0200

    added match expression for the listener

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,19 +1,26 @@
 use std::net::TcpListener;
 
-    pub struct Server {
-        addr: String,
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
     }
 
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
+    pub fn run(self) {
+        println!("Listening on {}", self.addr);
 
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
 
-            let listener = TcpListener::bind(&self.addr).unwrap();
-
-            loop {}
+        loop {
+            match listener.accept() {
+                Ok((_stream, _addr)) => {
+                    print!("Ok!");
+                }
+                Err(e) => print!("Failed to establish a connection: {}", e),
+            }
         }
     }
+}
