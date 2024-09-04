2856d378936525a87c4347ba3409cd3d31a214b5
andilar <andilar@gmail.com>
2024-09-04 17:41:42 +0200

    Chapter 38

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,5 +1,10 @@
-use std::net::TcpListener;
+use crate::http::request;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::f32::consts::E;
 use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -18,8 +23,19 @@ impl Server {
         loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
-                    let a = [1,2,3,4,5];
-                    //stream.read();
+                    let a = [1, 2, 3, 4, 5];
+                    let mut buffer = [0; 1024];
+
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                        }
+                        Err(e) => println!("Failed to read from  connection: {}", e),
+                    }
                 }
                 Err(e) => print!("Failed to establish a connection: {}", e),
             }
