50dbbf9cb139ab4c17a79b1abacc8d426f646c40
andilar <andilar@gmail.com>
2024-09-10 16:08:52 +0200

    Chapter 41

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,7 +1,9 @@
 use super::method::Method;
+use core::str;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -14,8 +16,20 @@ impl TryFrom<&[u8]> for Request {
 
     // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
 }
 
 pub enum ParseError {
@@ -33,6 +47,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
