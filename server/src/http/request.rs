e77b6918b87b32e23f3a9cad0d2130deea8b28ca
andilar <andilar@gmail.com>
2024-11-12 10:11:16 +0100

    updating

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -8,8 +8,8 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str::Utf8Error;
 
 pub struct Request {
-    path: String,
-    query_string: Option<String>,
+    path: &str,
+    query_string: Option<&str>,
     method: Method,
 }
 
@@ -32,11 +32,15 @@ impl TryFrom<&[u8]> for Request {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(path[i + 1..]);
             path = &path[..i];
         }
 
-        unimplemented!()
+        Ok(Self {
+            path: path,
+            query_string,
+            method,
+        })
     }
 }
 
