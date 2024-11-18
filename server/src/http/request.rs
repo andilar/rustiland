60827637e1d6f8882c47e7737fdfb947c5c5e1d4
andilar <andilar@gmail.com>
2024-11-18 14:21:40 +0100

    fixed string issue

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -8,8 +8,8 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str::Utf8Error;
 
 pub struct Request {
-    path: &str,
-    query_string: Option<&str>,
+    path: String,
+    query_string: Option<String>,
     method: Method,
 }
 
@@ -21,7 +21,8 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request): (&str, &str) =
+            get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -32,12 +33,12 @@ impl TryFrom<&[u8]> for Request {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(path[i + 1..]);
+            query_string = Some(path[i + 1..].to_string());
             path = &path[..i];
         }
 
         Ok(Self {
-            path: path,
+            path: path.to_string(),
             query_string,
             method,
         })
