bb83ff9885b362b19a6a636ba3ae6040425ff443
andilar <andilar@gmail.com>
2024-11-19 11:23:09 +0100

    Added query string handling with HashMaps

--- server/src/http/query_strings.rs
+++ server/src/http/query_strings.rs
@@ -16,3 +16,29 @@ impl<'buf> QueryString<'buf> {
         self.data.get(key)
     }
 }
+
+// a=1&b=2&c&d=&e===&d=7&d=abc
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(s: &'buf str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&') {
+            let mut key = sub_str;
+            let mut val = "";
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                val = &sub_str[i + 1..];
+            }
+
+            data.entry(key)
+                .and_modify(|existing: &mut Value| match existing {
+                    Value::Single(prev_val) => {
+                        *existing = Value::Multiple(vec![prev_val, val]);
+                    }
+                    Value::Multiple(vec) => vec.push(val),
+                })
+                .or_insert(Value::Single(val));
+        }
+        QueryString { data }
+    }
+}
