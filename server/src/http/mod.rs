cef663b2da9ce47b9187efab4340c06d8824f372
andilar <andilar@gmail.com>
2024-11-19 14:38:08 +0100

    Added Status codes

--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -3,8 +3,10 @@ pub use query_strings::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
 pub use response::Response;
+pub use status_code::StatusCode;
 
 pub mod method;
 pub mod query_strings;
 pub mod request;
 pub mod response;
+pub mod status_code;
