7f1f1853730b31dc4d13d83b80fe496f62496ec2
andilar <andilar@gmail.com>
2024-11-22 11:31:52 +0100

    added website handler

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,17 +2,16 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-use http::Method;
-use http::Request;
-
-mod server;
 use server::Server;
+use website_handler::WebsiteHandler;
 
 mod http;
+mod server;
+mod website_handler;
 
 fn main() {
     let server_address: String = String::from("127.0.0.1:8080");
     let server: Server = Server::new(server_address);
 
-    server.run();
+    server.run(WebsiteHandler);
 }
