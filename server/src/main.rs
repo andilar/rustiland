fdb63368a076825cfe0476a6df2c3aaa611f33cb
andilar <andilar@gmail.com>
2024-11-27 15:59:28 +0100

    added html files to stream, also serving them.

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,7 @@
 #![allow(unused_variables)]
 
 use server::Server;
+use std::{default, env};
 use website_handler::WebsiteHandler;
 
 mod http;
@@ -10,8 +11,12 @@ mod server;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));//backaslash?
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
+    println!("public path {}", public_path);
     let server_address: String = String::from("127.0.0.1:8080");
     let server: Server = Server::new(server_address);
 
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
